f01b5dfab251ca497039e6b8427ed2739dfe4cd0
bryanbocio <alexanderbocio@gmail.com>
2023-01-30 15:13:13 -0400

    trying http client

--- oswald-shop-ui/src/app/app.component.ts
+++ oswald-shop-ui/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Oswald';
+  products:any[];
+  constructor(private httpClient:HttpClient){}
+
+  ngOnInit(): void {
+    this.httpClient.get('https://localhost:5001/api/products?pageSize=50').subscribe((response:any)=>{
+          console.log(response)
+          this.products=response.data
+    },error=>{
+      console.log(error)
+    });
+  }
+
+ 
+
+
+
 }
