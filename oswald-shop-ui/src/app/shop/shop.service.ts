54cde3d594dee5e03131eff3c8bce32b0221bec7
bryanbocio <alexanderbocio@gmail.com>
2023-02-04 14:07:59 -0400

    creating shop service and the first method for getting all products from API

--- oswald-shop-ui/src/app/shop/shop.service.ts
+++ oswald-shop-ui/src/app/shop/shop.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Pagination } from '../shared/models/pagination';
+import { Product } from '../shared/models/products';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
+
+  getProducts():Observable<Pagination<Product[]>>{
+    return this.httpClient.get<Pagination<Product[]>>(environment.API_URL.concat('products'))
+  }
+
 }
