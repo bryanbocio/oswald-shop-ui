f10ca7e3b9fc85800872ca7dfd71ba98544d4396
bryanbocio <alexanderbocio@gmail.com>
2023-02-05 18:57:16 -0400

    implementing router link

--- oswald-shop-ui/src/app/app.module.ts
+++ oswald-shop-ui/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {HttpClientModule} from '@angular/common/http'
 import { CoreModule } from './core/core.module';
 import { ShopModule } from './shop/shop.module';
+import { HomeModule } from './home/home.module';
 @NgModule({
   declarations: [
     AppComponent
@@ -19,7 +20,8 @@ import { ShopModule } from './shop/shop.module';
     FontAwesomeModule,
     HttpClientModule,
     CoreModule,
-    ShopModule
+    ShopModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
