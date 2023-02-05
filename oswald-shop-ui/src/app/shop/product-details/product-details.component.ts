c1f27f5bb49d5320b002ddf593bcc6a5139763d8
bryanbocio <alexanderbocio@gmail.com>
2023-02-05 19:25:36 -0400

    designing product details UI

--- oswald-shop-ui/src/app/shop/product-details/product-details.component.ts
+++ oswald-shop-ui/src/app/shop/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ShopService } from '../shop.service';
+import { Product } from 'src/app/shared/models/products';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-details.component.scss']
 })
 export class ProductDetailsComponent implements OnInit {
+  product?:Product;
 
-  constructor() { }
+  constructor(private shopService:ShopService, private activatedRoute:ActivatedRoute,) { }
 
   ngOnInit(): void {
+    this.loadProduct()
+  }
+
+  loadProduct(){
+    const productId=this.activatedRoute.snapshot.paramMap.get('id');
+    if(productId) this.shopService.getProductById(+productId).subscribe({
+      next: product=>this.product=product,
+      error:error=>console.log(error)
+    });
   }
 
 }
