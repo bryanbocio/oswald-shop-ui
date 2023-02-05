d4fe8fa003bea297bbd754f9f6805651d5c72ff5
bryanbocio <alexanderbocio@gmail.com>
2023-02-05 10:47:06 -0400

    implementing methods from Shop services for getting brands and types

--- oswald-shop-ui/src/app/shop/shop.component.ts
+++ oswald-shop-ui/src/app/shop/shop.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ShopService } from './shop.service';
 import { Product } from '../shared/models/products';
+import { Brand } from '../shared/models/brand';
+import { Type } from '../shared/models/type';
 
 @Component({
   selector: 'app-shop',
@@ -9,19 +11,53 @@ import { Product } from '../shared/models/products';
   ]
 })
 export class ShopComponent implements OnInit {
+  //Arrays where store information
   products:Product[]=[];
-
+  brands:Brand[]=[];
+  types:Type[]=[];
+  
+  //variables for sending any params
+  brandIdSelected=0;
+  typeIdSelected=0;
 
   constructor(private shopService:ShopService) { }
 
   ngOnInit(): void {
     this.loadProducts();
+    this.loadBrands();
+    this.loadTypes();
   }
 
   loadProducts(){
-    this.shopService.getProducts().subscribe({
+    this.shopService.getProducts(this.brandIdSelected,this.typeIdSelected).subscribe({
       next:response=> this.products=response.data,
       error:error=>console.log(error)
     })
+  }
+
+
+  loadBrands(){
+    this.shopService.getBrands().subscribe({
+        next:response=> this.brands=[{id:0, name:'All'},...response],
+        error:error=>console.log(error)
+    });
+  }
+
+  loadTypes(){
+    this.shopService.getTypes().subscribe({
+        next:response=> this.types=response=[{id:0, name:'All'},...response],
+        error:error=>console.log(error)
+    });
+  }
+
+
+  onBrandSelected(brandId:number){
+    this.brandIdSelected=brandId;
+    this.loadProducts();
+  }
+
+  onTypeSelected(typeId:number){
+    this.typeIdSelected=typeId;
+    this.loadProducts();
   }
 }
