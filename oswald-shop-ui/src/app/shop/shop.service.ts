08628d89b6c02eb00cc24c6761b4c91d7b03cba6
bryanbocio <alexanderbocio@gmail.com>
2023-02-05 10:46:31 -0400

    adding get methods for brands and types, and sending params for filtering by brands and types

--- oswald-shop-ui/src/app/shop/shop.service.ts
+++ oswald-shop-ui/src/app/shop/shop.service.ts
@@ -1,9 +1,11 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Pagination } from '../shared/models/pagination';
 import { Product } from '../shared/models/products';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
+import { Brand } from '../shared/models/brand';
+import { Type} from '../shared/models/type';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +14,20 @@ export class ShopService {
 
   constructor(private httpClient:HttpClient) { }
 
-  getProducts():Observable<Pagination<Product[]>>{
-    return this.httpClient.get<Pagination<Product[]>>(environment.API_URL.concat('products'))
+  getProducts(brandId?:number, typeId?:number):Observable<Pagination<Product[]>>{
+    let params=new HttpParams();
+
+    if(brandId) params=params.append('brandId',brandId);
+    if(typeId) params=params.append('typeId',typeId);
+
+    return this.httpClient.get<Pagination<Product[]>>(environment.API_URL.concat('products'), {params});
   }
 
+  getBrands(){
+      return this.httpClient.get<Brand[]>(environment.API_URL.concat('products/brands'));
+  }
+
+  getTypes():Observable<Type[]>{
+    return this.httpClient.get<Type[]>(environment.API_URL.concat('products/product-types'));
+}
 }
