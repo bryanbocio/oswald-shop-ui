e3b6288e1ed19f6bf836d970b0e4c88dda86be64
bryanbocio <alexanderbocio@gmail.com>
2023-02-05 12:10:32 -0400

    adding a new interfaces for store params for sending to API

--- oswald-shop-ui/src/app/shop/shop.service.ts
+++ oswald-shop-ui/src/app/shop/shop.service.ts
@@ -6,6 +6,7 @@ import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
 import { Brand } from '../shared/models/brand';
 import { Type} from '../shared/models/type';
+import { ShopParams } from '../shared/models/shopParams';
 
 @Injectable({
   providedIn: 'root'
@@ -14,11 +15,17 @@ export class ShopService {
 
   constructor(private httpClient:HttpClient) { }
 
-  getProducts(brandId?:number, typeId?:number):Observable<Pagination<Product[]>>{
+  getProducts(shopParams:ShopParams):Observable<Pagination<Product[]>>{
     let params=new HttpParams();
 
-    if(brandId) params=params.append('brandId',brandId);
-    if(typeId) params=params.append('typeId',typeId);
+    if(shopParams.brandId>0) params=params.append('brandId',shopParams.brandId);
+    if(shopParams.typeId) params=params.append('typeId',shopParams.typeId);
+    if(shopParams.search)params=params.append('search',shopParams.search)
+    
+    params=params.append('sort',shopParams.sort);
+    params=params.append('pageIndex',shopParams.pageNumber);
+    params=params.append('pageSize',shopParams.pageSize);
+
 
     return this.httpClient.get<Pagination<Product[]>>(environment.API_URL.concat('products'), {params});
   }
