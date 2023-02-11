9f1eee48fd0a2194358a50765c5223f6171c2369
bryanbocio <alexanderbocio@gmail.com>
2023-02-11 16:58:34 -0400

    using breadcrumb service in product details

--- oswald-shop-ui/src/app/shop/product-details/product-details.component.ts
+++ oswald-shop-ui/src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ShopService } from '../shop.service';
 import { Product } from 'src/app/shared/models/products';
 import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
@@ -11,7 +12,7 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductDetailsComponent implements OnInit {
   product?:Product;
 
-  constructor(private shopService:ShopService, private activatedRoute:ActivatedRoute,) { }
+  constructor(private shopService:ShopService, private activatedRoute:ActivatedRoute, private breadCrumbService:BreadcrumbService) { }
 
   ngOnInit(): void {
     this.loadProduct()
@@ -20,7 +21,10 @@ export class ProductDetailsComponent implements OnInit {
   loadProduct(){
     const productId=this.activatedRoute.snapshot.paramMap.get('id');
     if(productId) this.shopService.getProductById(+productId).subscribe({
-      next: product=>this.product=product,
+      next: product=>{
+        this.product=product;
+        this.breadCrumbService.set('@productDetails',product.name)
+      },
       error:error=>console.log(error)
     });
   }
