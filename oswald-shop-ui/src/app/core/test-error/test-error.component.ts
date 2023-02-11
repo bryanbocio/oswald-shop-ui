ecc66be98c6df717bfa625091fd91d20323eb219
bryanbocio <alexanderbocio@gmail.com>
2023-02-11 15:11:59 -0400

    adding not found and server error components

--- oswald-shop-ui/src/app/core/test-error/test-error.component.ts
+++ oswald-shop-ui/src/app/core/test-error/test-error.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-test-error',
@@ -6,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test-error.component.scss']
 })
 export class TestErrorComponent implements OnInit {
+  baseUrl=environment.API_URL;
 
-  constructor() { }
+
+
+  constructor(private httpClient:HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  get404Error(){
+    this.httpClient.get(this.baseUrl.concat('products/345')).subscribe({
+      next: response=> console.log(response),
+      error:error=>console.log(error)
+    });
+  }
+
+  get500Error(){
+    this.httpClient.get(this.baseUrl.concat('buggy/servererror')).subscribe({
+      next: response=> console.log(response),
+      error:error=>console.log(error)
+    });
+  }
+
+  get400Error(){
+    this.httpClient.get(this.baseUrl.concat('buggy/badrequest')).subscribe({
+      next: response=> console.log(response),
+      error:error=>console.log(error)
+    });
+  }
+
+  get400ValidationError(){
+    this.httpClient.get(this.baseUrl.concat('products/fortytwo')).subscribe({
+      next: response=> console.log(response),
+      error:error=>console.log(error)
+    });
   }
 
 }
