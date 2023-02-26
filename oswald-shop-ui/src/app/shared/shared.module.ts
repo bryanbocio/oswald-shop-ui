f9f34d27a5b3ebb64a7cb6d9cfa38f4fd291c1d5
bryanbocio <alexanderbocio@gmail.com>
2023-02-26 10:29:22 -0400

    adding slider for home component

--- oswald-shop-ui/src/app/shared/shared.module.ts
+++ oswald-shop-ui/src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import {PaginationModule} from 'ngx-bootstrap/pagination';
 import { PaginationHeaderComponent } from './pagination-header/pagination-header.component';
 import { PagerComponent } from './pager/pager.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel'
 
 
 @NgModule({
@@ -13,10 +14,12 @@ import { PagerComponent } from './pager/pager.component';
   imports: [
     CommonModule,
     PaginationModule.forRoot(),
+    CarouselModule.forRoot(),
   ],exports:[
     PaginationModule,
     PaginationHeaderComponent,
-    PagerComponent
+    PagerComponent,
+    CarouselModule
   ]
 })
 export class SharedModule { }
