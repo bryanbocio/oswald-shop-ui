317f42a7116e7f4218a68b16ea0caeb0dc445f70
bryanbocio <alexanderbocio@gmail.com>
2023-02-27 20:49:05 -0400

    getting total item's count from redis

--- oswald-shop-ui/src/app/core/nav-bar/nav-bar.component.ts
+++ oswald-shop-ui/src/app/core/nav-bar/nav-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BasketService } from 'src/app/basket/basket.service';
+import { BasketItem } from 'src/app/shared/models/basket';
 
 @Component({
   selector: 'app-nav-bar',
@@ -11,6 +12,10 @@ export class NavBarComponent implements OnInit {
 constructor(public basketService:BasketService) { }
 
   ngOnInit(): void {
+  }
+
+  getCount(items:BasketItem[]){
+    return items.reduce((sum,item)=> sum + item.quantity, 0);
   }
 
 }
