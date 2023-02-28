a4e1f7f96a9cfe9be904fc0e8df0ebae907c33c4
bryanbocio <alexanderbocio@gmail.com>
2023-02-27 20:58:49 -0400

    adding calculator total method in basket service

--- oswald-shop-ui/src/app/basket/basket.service.ts
+++ oswald-shop-ui/src/app/basket/basket.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { Basket, BasketItem } from '../shared/models/basket';
+import { Basket, BasketItem, BasketTotals } from '../shared/models/basket';
 import { Product } from '../shared/models/products';
 
 @Injectable({
@@ -14,19 +14,29 @@ export class BasketService {
   private basketSource= new BehaviorSubject<Basket | null>(null);
   basketSource$= this.basketSource.asObservable();
 
+  private basketTotalSource= new BehaviorSubject<BasketTotals | null>(null);
+  basketTotalSource$=this.basketTotalSource.asObservable();
+
+
   constructor(private httpClient: HttpClient) { }
 
 
   getBasket(id:string){
     return this.httpClient.get<Basket>(this.baseUrl.concat('basket?id='+id)).subscribe({
-      next:basket=>this.basketSource.next(basket)
+      next:basket=>{
+        this.basketSource.next(basket);
+        this.calculateTotals();
+      }
     })
   }
 
 
   setBasket(basket:Basket){
     return this.httpClient.post<Basket>(this.baseUrl.concat('basket'), basket).subscribe({
-      next:basket=>this.basketSource.next(basket)
+      next:basket=>{
+        this.basketSource.next(basket);
+        this.calculateTotals();
+      }
     })
   }
 
@@ -73,5 +83,17 @@ export class BasketService {
       brand:item.productBrand,
       type:item.productType,
     }
+  }
+
+
+  private calculateTotals(){
+    const basket=this.getCurrentBasketValue();
+
+    if(!basket) return;
+
+    const shipping=0;
+    const subTotal=basket.items.reduce((previewsValue, currentValue)=>(currentValue.price * currentValue.quantity) + previewsValue,0);
+    const total=subTotal + shipping;
+    this.basketTotalSource.next({shipping, subTotal, total});
   }
 }
