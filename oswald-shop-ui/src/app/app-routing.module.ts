2aae1042590693c6bc5e87ffdbb9eba3fb204eda
bryanbocio <alexanderbocio@gmail.com>
2023-04-01 16:54:10 -0400

    implementing authGuard

--- oswald-shop-ui/src/app/app-routing.module.ts
+++ oswald-shop-ui/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { TestErrorComponent } from './core/test-error/test-error.component';
 import { NotFoundComponent } from './core/not-found/not-found.component';
 import { ServerErrorComponent } from './core/server-error/server-error.component';
+import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
   {path:'', component:HomeComponent,data:{breadcrumb:'Home'}},
@@ -12,7 +13,12 @@ const routes: Routes = [
   {path:'server-error', component:ServerErrorComponent},
   {path:'shop', loadChildren:()=> import('./shop/shop.module').then(module=>module.ShopModule)},
   {path:'basket', loadChildren:()=> import('./basket/basket.module').then(module=>module.BasketModule)},
-  {path:'checkout', loadChildren:()=> import('./checkout/checkout.module').then(module=>module.CheckoutModule)},
+ 
+  {
+    path:'checkout', 
+    canActivate:[AuthGuard],
+    loadChildren:()=> import('./checkout/checkout.module').then(module=>module.CheckoutModule)},
+ 
   {path:'account', loadChildren:()=> import('./account/account.module').then(module=>module.AccountModule)},
   {path:'**', redirectTo:'',pathMatch:'full'},
 
