19279721852d7bdb07766f92e787b29dfd8e4d82
bryanbocio <alexanderbocio@gmail.com>
2023-04-01 16:54:44 -0400

    changing the observable type, from Behavior to ReplaySubject

--- oswald-shop-ui/src/app/account/account.service.ts
+++ oswald-shop-ui/src/app/account/account.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { BehaviorSubject } from 'rxjs';
+import {  ReplaySubject, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { User } from '../shared/models/user';
@@ -12,19 +12,28 @@ import { User } from '../shared/models/user';
 export class AccountService {
   baseUrl=environment.API_URL;
 
-  private currentUserSource=new BehaviorSubject<User | null>(null);
+  private currentUserSource=new ReplaySubject<User | null>(1);
   currentUser$=this.currentUserSource.asObservable();
 
   constructor(private httpClient:HttpClient, private router:Router) { }
 
-  loadCurrentUser(token:String){
+  loadCurrentUser(token:String | null){
+    if(token===null) {
+      this.currentUserSource.next(null);
+      return of(null);
+    }
     let headers= new HttpHeaders();
     headers= headers.set('Authorization', `Bearer ${token}`)
 
     return this.httpClient.get<User>(this.baseUrl+'account', {headers}).pipe(
         map(user=>{
+         if(user){
           localStorage.setItem('token',user.token);
           this.currentUserSource.next(user);
+          return user;
+        }else{
+          return null
+         }
         })
 
     )
