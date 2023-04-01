f5370bd6366d31fee009b1ed492bc95f8503c6bb
bryanbocio <alexanderbocio@gmail.com>
2023-04-01 16:55:24 -0400

    implementing register from into login component, i have  to rename this component bc there are 2 components

--- oswald-shop-ui/src/app/account/login/login.component.ts
+++ oswald-shop-ui/src/app/account/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { AbstractControl, AsyncValidatorFn, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AccountService } from '../account.service';
+import { ToastrService } from 'ngx-toastr';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-login',
@@ -12,33 +14,53 @@ import { AccountService } from '../account.service';
 export class LoginComponent implements OnInit {
 
   container:HTMLElement= document.getElementById("container");
-  containerClass:String=''
+  containerClass:string=''
+
+  returnUrl:string;
+
+  errors:string[] | null = null;
+
+  complexPassword="(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$";
 
   loginForm= new FormGroup({
-    email:new FormControl('', Validators.required),
-    password:new FormControl('', Validators.required),
+    email:new FormControl('',[ Validators.required, Validators.email]),
+    password:new FormControl('', [Validators.required, ]),
   });
 
-  
+  registerForm= this.formBuilder.group({
+    displayName:['',Validators.required],
+    email:['', [Validators.required, Validators.email]],
+    password:['',[Validators.required, Validators.pattern(this.complexPassword)]]
+  })
 
+  constructor(private accountService:AccountService,
+              private router:Router, 
+              private formBuilder:FormBuilder,
+              private toastService:ToastrService,
+              private activatedRoute:ActivatedRoute) {
+                this.returnUrl=this.activatedRoute.snapshot.queryParams['returnUrl'] || '/shop';
 
-  constructor(private accountService:AccountService, private router:Router) { }
+               }
 
   ngOnInit(): void { 
   }
 
+  
   animate(){
-   this.containerClass='sign-up-mode';
+   this.containerClass===''? this.containerClass='sign-up-mode': this.containerClass=''; 
   }
-
-  dontAnimate(){
-    this.containerClass='';
-  }
-
 
   onSubmit(){
     this.accountService.login(this.loginForm.value).subscribe({
-      next:()=> this.router.navigateByUrl('/shop')
+      next:()=> this.router.navigateByUrl(this.returnUrl)
+    })
+  }
+
+
+  onSubmitRegister(){
+    this.accountService.register(this.registerForm.value).subscribe({
+      next:() => this.router.navigateByUrl('/shop'),
+      error:error => this.toastService.error(error.errors) 
     })
   }
 
